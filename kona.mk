f6d81f145530813d42164e0b4a9979fcbbb48a13
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-08-17 04:16:24 +0200

    sm8250-common: audio: Use AOSP USB v2 audio HAL
    Change-Id: I579e442a8c41ec62acc1e5be7478889bf5bc24b8

--- kona.mk
+++ kona.mk
@@ -159,8 +159,7 @@ PRODUCT_PACKAGES += \
     audio.primary.default \
     audio.primary.kona \
     audio.r_submix.default \
-    audio.usb.default \
-    audio.usbv2.default
+    audio.usbv2.default \
 
 PRODUCT_PACKAGES += \
     liba2dpoffload \
