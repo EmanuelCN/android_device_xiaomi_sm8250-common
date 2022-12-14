09ceeeacb829be1db8ddee27dc3429425f67bf6c
EmanuelCN <emanuelgithub@gmail.com>
2022-12-14 23:34:48 +0000

    sm8250-common: Rename libperfmgr build target
    With it add the soong namespaces

--- kona.mk
+++ kona.mk
@@ -437,7 +437,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi-libperfmgr
+    android.hardware.power-service.xiaomi-sm8250-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json \
@@ -523,6 +523,8 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel \
     hardware/xiaomi \
     hardware/qcom-caf/wlan
 
